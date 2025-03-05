74688debddd0128cb5f5c152b9cc16eb2f488b7d
megrose10 <157438545+megrose10@users.noreply.github.com>
2025-03-05 00:03:40 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -6,26 +6,26 @@ eval(fs.readFileSync('code.js') + '');
 
 function test() {
     let adjListGraph = {
-        'A': {'B', 'C'},
-        'B': {'A', 'C'},
-        'C': {'A', 'B'}
+        'A': ['B', 'C'],
+        'B': ['A', 'C'],
+        'C': ['A', 'B']
     };
     let res1 = hasCycle(adjListGraph);
     assert.deepStrictEqual(res1, true);
 
     let adjListGraph2 = {
-        'A': {'B', 'C'},
-        'B': {'A', 'C', 'D'},
-        'C': {'A', 'B', 'D'},
-        'D': {'B', 'C'}
+        'A': ['B', 'C'],
+        'B': ['A', 'C', 'D'],
+        'C': ['A', 'B', 'D'],
+        'D': ['B', 'C']
     };
     let res2 = hasCycle(adjListGraph2);
     assert.deepStrictEqual(res2, true);
 
     let adjListGraph3 = {
-        'A': {'B'},
-        'B': {'A'},
-        'C': {}
+        'A': ['B'],
+        'B': ['A'],
+        'C': []
     };
     let res3 = hasCycle(adjListGraph3);
     assert.deepStrictEqual(res3, false);
